c20b66f32e3cc61477a5be75be4fa6c5737ee2f3
Mario <spindizzy@outlook.com>
2026-05-08 14:59:25 +0200

    feat: exposes tools

--- src/main/java/org/openweather/Main.java
+++ src/main/java/org/openweather/Main.java
@@ -1,8 +1,11 @@
 package org.openweather;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -12,4 +15,8 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+        return  MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+    }
 }
