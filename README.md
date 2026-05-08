416e01fd3c9ce1ef8b47bbd53fc17471061b446f
Mario <spindizzy@outlook.com>
2026-05-08 16:14:12 +0200

    turns banner off with configuration

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # open-weather
-This is a service that exposes a MCP endpoint to request 
+This is a service that exposes a MCP server to request 
 weather information from
 [Openweathermap](https://openweathermap.org).
-
-The main purpose is to serve as a simple MCP server for MCP clients.
 
 It requires an API Key from Openweathermap.
 
--- src/main/java/org/openweather/Main.java
+++ src/main/java/org/openweather/Main.java
@@ -2,7 +2,6 @@ package org.openweather;
 
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,9 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        SpringApplication.run(Main.class, args);
     }
 
     @Bean
