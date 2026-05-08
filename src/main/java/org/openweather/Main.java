b5aff90e50bf278c8c80f62dbeb557f6eda7a1c4
Mario <spindizzy@outlook.com>
2026-05-08 16:09:18 +0200

    disables banner and extensive logging to fix issues with MCP

--- src/main/java/org/openweather/Main.java
+++ src/main/java/org/openweather/Main.java
@@ -2,6 +2,7 @@ package org.openweather;
 
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,7 +13,9 @@ import org.springframework.context.annotation.Bean;
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
     @Bean
